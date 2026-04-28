55c509b46b639ae7491cd1faaaee0d49426d4f70
Tomáš Pečinka <tomas-pecinka@seznam.cz>
2026-04-28 18:33:12 +0200

    feat: add remote user control for expression tree viewer

---
+++ ExpressionTreeExplorer.Vsix/ExpressionTreeControl.cs
@@ -0,0 +1,12 @@
+﻿using ExpressionTreeExplorer.Core;
+
+using Microsoft.VisualStudio.Extensibility.UI;
+
+namespace ExpressionTreeExplorer.Vsix;
+
+internal class ExpressionTreeControl : RemoteUserControl
+{
+    public ExpressionTreeControl(ExpressionPayload dataContext) : base(dataContext)
+    {
+    }
+}
