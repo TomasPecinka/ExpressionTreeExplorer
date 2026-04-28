e100bad86885f8ce4030cd301f789989d3090cbf
Tomáš Pečinka <tomas-pecinka@seznam.cz>
2026-04-28 17:57:24 +0200

    feat: add expression DTO models

---
+++ ExpressionTreeExplorer.Core/ExpressionNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ExpressionTreeExplorer.Core;
+
+[DataContract]
+public class ExpressionNode
+{
+    [DataMember]
+    public string Display { get; set; } = "";
+
+    [DataMember]
+    public string TypeDisplay { get; set; } = "";
+
+    [DataMember]
+    public List<ExpressionNode> Children { get; set; } = new();
+}
