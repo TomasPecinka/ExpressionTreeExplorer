c2d19a634caaf8c5a2b7ea3b5871adf6ad4dd55a
Tomáš Pečinka <tomas-pecinka@seznam.cz>
2026-04-28 18:11:06 +0200

    feat: register debugger visualizer provider for expressions

---
+++ ExpressionTreeExplorer.Vsix/ExpressionTreeVisualizerProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+
+using ExpressionTreeExplorer.Core;
+using ExpressionTreeExplorer.ObjectSource;
+
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.DebuggerVisualizers;
+using Microsoft.VisualStudio.RpcContracts.RemoteUI;
+
+namespace ExpressionTreeExplorer.Vsix;
+
+[VisualStudioContribution]
+internal class ExpressionTreeVisualizerProvider : DebuggerVisualizerProvider
+{
+    public override DebuggerVisualizerProviderConfiguration DebuggerVisualizerProviderConfiguration =>
+        new(new VisualizerTargetType("Expression Explorer", typeof(LambdaExpression)))
+        {
+            VisualizerObjectSourceType = new(typeof(ExpressionVisualizerObjectSource))
+        };
+
+    public override async Task<IRemoteUserControl> CreateVisualizerAsync(VisualizerTarget visualizerTarget, CancellationToken cancellationToken)
+    {
+        var payload = await visualizerTarget.ObjectSource
+            .RequestDataAsync<ExpressionPayload>(null, cancellationToken);
+
+        return new ExpressionTreeControl(payload);
+    }
+}
