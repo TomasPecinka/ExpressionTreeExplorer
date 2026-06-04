e82b2d21bb52466c6a575fc77125c3a8ea18a154
Tomáš Pečinka <tomas-pecinka@seznam.cz>
2026-06-04 21:27:30 +0200

    feat: add support for new expression types and extend node metadata

--- ExpressionTreeExplorer.Core/ExpressionNodeBuilder.cs
+++ ExpressionTreeExplorer.Core/ExpressionNodeBuilder.cs
@@ -25,12 +25,26 @@ public static class ExpressionNodeBuilder
         {
             LambdaExpression lambda => BuildLambda(lambda, path),
             BinaryExpression binary => BuildBinary(binary, path),
+            ConditionalExpression conditional => BuildConditional(conditional, path),
             MemberExpression member => BuildMember(member, path),
             ConstantExpression constant => BuildConstant(constant, path),
             ParameterExpression parameter => BuildParameter(parameter, path),
             MethodCallExpression methodCall => BuildMethodCall(methodCall, path),
             UnaryExpression unary => BuildUnary(unary, path),
             MemberInitExpression memberInit => BuildMemberInit(memberInit, path),
+            ListInitExpression listInit => BuildListInit(listInit, path),
+            NewExpression newExpr => BuildNew(newExpr, path),
+            NewArrayExpression newArray => BuildNewArray(newArray, path),
+            TypeBinaryExpression typeBinary => BuildTypeBinary(typeBinary, path),
+            InvocationExpression invocation => BuildInvocation(invocation, path),
+            IndexExpression index => BuildIndex(index, path),
+            DefaultExpression defaultExpr => BuildDefault(defaultExpr, path),
+            BlockExpression block => BuildBlock(block, path),
+            TryExpression tryExpr => BuildTry(tryExpr, path),
+            SwitchExpression switchExpr => BuildSwitch(switchExpr, path),
+            GotoExpression gotoExpr => BuildGoto(gotoExpr, path),
+            LabelExpression label => BuildLabel(label, path),
+            LoopExpression loop => BuildLoop(loop, path),
 
             _ => BuildFallback(expr, path)
         };
@@ -45,6 +59,11 @@ public static class ExpressionNodeBuilder
             TypeDisplay = SimplifyType(lambda.Type),
             Kind = "Lambda",
             NodeType = lambda.NodeType.ToString(),
+            Details =
+            [
+                Detail("ReturnType", SimplifyType(lambda.ReturnType)),
+                Detail("ParameterCount", lambda.Parameters.Count.ToString())
+            ],
             Children =
             [
                 BuildNode(lambda.Body, $"{path}/0")
@@ -61,6 +80,11 @@ public static class ExpressionNodeBuilder
             TypeDisplay = binary.Type.Name,
             Kind = "Binary",
             NodeType = binary.NodeType.ToString(),
+            Details =
+            [
+                Detail("IsLifted", binary.IsLifted.ToString()),
+                Detail("Method", binary.Method?.Name ?? "-")
+            ],
             Children =
             [
                 BuildNode(binary.Left, $"{path}/0"),
@@ -78,6 +102,11 @@ public static class ExpressionNodeBuilder
             TypeDisplay = member.Type.Name,
             Kind = "Member",
             NodeType = member.NodeType.ToString(),
+            Details =
+            [
+                Detail("DeclaringType", member.Member.DeclaringType?.Name ?? "-"),
+                Detail("MemberType", member.Member.MemberType.ToString())
+            ],
         };
 
         if (member.Expression is not null)
@@ -100,6 +129,11 @@ public static class ExpressionNodeBuilder
             TypeDisplay = constant.Type.Name,
             Kind = "Constant",
             NodeType = constant.NodeType.ToString(),
+            Details =
+            [
+                Detail("Value", FormatConstant(constant)),
+                Detail("ClrType", constant.Type.Name)
+            ],
         };
     }
 
@@ -112,6 +146,10 @@ public static class ExpressionNodeBuilder
             TypeDisplay = parameter.Type.Name,
             Kind = "Parameter",
             NodeType = parameter.NodeType.ToString(),
+            Details =
+            [
+                Detail("IsByRef", parameter.IsByRef.ToString())
+            ],
         };
     }
 
@@ -219,6 +257,436 @@ public static class ExpressionNodeBuilder
                 Detail("BindingsCount", mi.Bindings.Count.ToString())
             ],
             Children = children
+        };
+    }
+
+    private static ExpressionNode BuildConditional(ConditionalExpression c, string path)
+    {
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Conditional",
+            NodeType = c.NodeType.ToString(),
+            Display = "Conditional",
+            TypeDisplay = SimplifyType(c.Type),
+            Details =
+            [
+                Detail("Type", SimplifyType(c.Type))
+            ],
+            Children =
+            [
+                BuildNode(c.Test, $"{path}/0"),
+                BuildNode(c.IfTrue, $"{path}/1"),
+                BuildNode(c.IfFalse, $"{path}/2")
+            ]
+        };
+    }
+
+    private static ExpressionNode BuildNew(NewExpression n, string path)
+    {
+        var children = new List<ExpressionNode>();
+        for (var i = 0; i < n.Arguments.Count; i++)
+        {
+            children.Add(BuildNode(n.Arguments[i], $"{path}/{i}"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "New",
+            NodeType = n.NodeType.ToString(),
+            Display = $"new {SimplifyType(n.Type)}",
+            TypeDisplay = SimplifyType(n.Type),
+            Details =
+            [
+                Detail("Constructor", n.Constructor?.ToString() ?? "-"),
+                Detail("ArgumentsCount", n.Arguments.Count.ToString())
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildTypeBinary(TypeBinaryExpression t, string path)
+    {
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "TypeBinary",
+            NodeType = t.NodeType.ToString(),
+            Display = $"{t.NodeType} {SimplifyType(t.TypeOperand)}",
+            TypeDisplay = SimplifyType(t.Type),
+            Details =
+            [
+                Detail("TypeOperand", SimplifyType(t.TypeOperand))
+            ],
+            Children =
+            [
+                BuildNode(t.Expression, $"{path}/0")
+            ]
+        };
+    }
+
+    private static ExpressionNode BuildInvocation(InvocationExpression inv, string path)
+    {
+        var children = new List<ExpressionNode>
+        {
+            BuildNode(inv.Expression, $"{path}/0")
+        };
+        for (var i = 0; i < inv.Arguments.Count; i++)
+        {
+            children.Add(BuildNode(inv.Arguments[i], $"{path}/{i + 1}"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Invocation",
+            NodeType = inv.NodeType.ToString(),
+            Display = "Invoke",
+            TypeDisplay = SimplifyType(inv.Type),
+            Details =
+            [
+                Detail("ArgumentsCount", inv.Arguments.Count.ToString())
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildNewArray(NewArrayExpression na, string path)
+    {
+        var children = new List<ExpressionNode>();
+        for (var i = 0; i < na.Expressions.Count; i++)
+        {
+            children.Add(BuildNode(na.Expressions[i], $"{path}/{i}"));
+        }
+
+        var elementType = na.Type.GetElementType();
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "NewArray",
+            NodeType = na.NodeType.ToString(),
+            Display = $"{na.NodeType} {SimplifyType(elementType ?? na.Type)}",
+            TypeDisplay = SimplifyType(na.Type),
+            Details =
+            [
+                Detail("ElementType", SimplifyType(elementType ?? na.Type))
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildListInit(ListInitExpression li, string path)
+    {
+        var children = new List<ExpressionNode>
+        {
+            BuildNode(li.NewExpression, $"{path}/0")
+        };
+
+        for (var i = 0; i < li.Initializers.Count; i++)
+        {
+            var init = li.Initializers[i];
+            var initChildren = new List<ExpressionNode>();
+            for (var j = 0; j < init.Arguments.Count; j++)
+            {
+                initChildren.Add(BuildNode(init.Arguments[j], $"{path}/{i + 1}/{j}"));
+            }
+
+            children.Add(new ExpressionNode
+            {
+                Path = $"{path}/{i + 1}",
+                Kind = "ElementInit",
+                NodeType = "ElementInit",
+                Display = init.AddMethod.Name,
+                TypeDisplay = "",
+                Details =
+                [
+                    Detail("AddMethod", init.AddMethod.Name),
+                    Detail("ArgumentsCount", init.Arguments.Count.ToString())
+                ],
+                Children = initChildren
+            });
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "ListInit",
+            NodeType = li.NodeType.ToString(),
+            Display = "ListInit",
+            TypeDisplay = SimplifyType(li.Type),
+            Details =
+            [
+                Detail("InitializersCount", li.Initializers.Count.ToString())
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildIndex(IndexExpression ix, string path)
+    {
+        var children = new List<ExpressionNode>();
+        var index = 0;
+
+        if (ix.Object != null)
+        {
+            children.Add(BuildNode(ix.Object, $"{path}/{index++}"));
+        }
+
+        foreach (var arg in ix.Arguments)
+        {
+            children.Add(BuildNode(arg, $"{path}/{index++}"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Index",
+            NodeType = ix.NodeType.ToString(),
+            Display = ix.Indexer?.Name ?? "Item",
+            TypeDisplay = SimplifyType(ix.Type),
+            Details =
+            [
+                Detail("Indexer", ix.Indexer?.Name ?? "-")
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildDefault(DefaultExpression d, string path)
+    {
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Default",
+            NodeType = d.NodeType.ToString(),
+            Display = $"default({SimplifyType(d.Type)})",
+            TypeDisplay = SimplifyType(d.Type),
+            Details =
+            [
+                Detail("Type", SimplifyType(d.Type))
+            ],
+        };
+    }
+
+    private static ExpressionNode BuildBlock(BlockExpression b, string path)
+    {
+        var children = new List<ExpressionNode>();
+        var index = 0;
+
+        foreach (var variable in b.Variables)
+        {
+            children.Add(BuildNode(variable, $"{path}/{index++}"));
+        }
+
+        foreach (var expr in b.Expressions)
+        {
+            children.Add(BuildNode(expr, $"{path}/{index++}"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Block",
+            NodeType = b.NodeType.ToString(),
+            Display = "Block",
+            TypeDisplay = SimplifyType(b.Type),
+            Details =
+            [
+                Detail("ExpressionsCount", b.Expressions.Count.ToString()),
+                Detail("VariablesCount", b.Variables.Count.ToString())
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildTry(TryExpression t, string path)
+    {
+        var children = new List<ExpressionNode>
+        {
+            BuildNode(t.Body, $"{path}/0")
+        };
+
+        var index = 1;
+        foreach (var handler in t.Handlers)
+        {
+            var handlerChildren = new List<ExpressionNode>();
+            var handlerIndex = 0;
+
+            if (handler.Filter != null)
+            {
+                handlerChildren.Add(BuildNode(handler.Filter, $"{path}/{index}/{handlerIndex++}"));
+            }
+
+            handlerChildren.Add(BuildNode(handler.Body, $"{path}/{index}/{handlerIndex}"));
+
+            children.Add(new ExpressionNode
+            {
+                Path = $"{path}/{index}",
+                Kind = "CatchBlock",
+                NodeType = "CatchBlock",
+                Display = $"catch ({SimplifyType(handler.Test)})",
+                TypeDisplay = SimplifyType(handler.Test),
+                Details =
+                [
+                    Detail("ExceptionType", SimplifyType(handler.Test)),
+                    Detail("Variable", handler.Variable?.Name ?? "-")
+                ],
+                Children = handlerChildren
+            });
+            index++;
+        }
+
+        if (t.Finally != null)
+        {
+            children.Add(BuildNode(t.Finally, $"{path}/{index++}"));
+        }
+
+        if (t.Fault != null)
+        {
+            children.Add(BuildNode(t.Fault, $"{path}/{index}"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Try",
+            NodeType = t.NodeType.ToString(),
+            Display = "Try",
+            TypeDisplay = SimplifyType(t.Type),
+            Details =
+            [
+                Detail("HandlersCount", t.Handlers.Count.ToString()),
+                Detail("HasFinally", (t.Finally != null).ToString()),
+                Detail("HasFault", (t.Fault != null).ToString())
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildSwitch(SwitchExpression s, string path)
+    {
+        var children = new List<ExpressionNode>
+        {
+            BuildNode(s.SwitchValue, $"{path}/0")
+        };
+
+        var index = 1;
+        foreach (var c in s.Cases)
+        {
+            var caseChildren = new List<ExpressionNode>();
+            var caseIndex = 0;
+
+            foreach (var testValue in c.TestValues)
+            {
+                caseChildren.Add(BuildNode(testValue, $"{path}/{index}/{caseIndex++}"));
+            }
+
+            caseChildren.Add(BuildNode(c.Body, $"{path}/{index}/{caseIndex}"));
+
+            children.Add(new ExpressionNode
+            {
+                Path = $"{path}/{index}",
+                Kind = "SwitchCase",
+                NodeType = "SwitchCase",
+                Display = "case",
+                TypeDisplay = "",
+                Details =
+                [
+                    Detail("TestValuesCount", c.TestValues.Count.ToString())
+                ],
+                Children = caseChildren
+            });
+            index++;
+        }
+
+        if (s.DefaultBody != null)
+        {
+            children.Add(BuildNode(s.DefaultBody, $"{path}/{index}"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Switch",
+            NodeType = s.NodeType.ToString(),
+            Display = "Switch",
+            TypeDisplay = SimplifyType(s.Type),
+            Details =
+            [
+                Detail("CasesCount", s.Cases.Count.ToString()),
+                Detail("HasDefault", (s.DefaultBody != null).ToString())
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildGoto(GotoExpression g, string path)
+    {
+        var children = new List<ExpressionNode>();
+        if (g.Value != null)
+        {
+            children.Add(BuildNode(g.Value, $"{path}/0"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Goto",
+            NodeType = g.NodeType.ToString(),
+            Display = $"{g.Kind} {g.Target.Name}",
+            TypeDisplay = SimplifyType(g.Type),
+            Details =
+            [
+                Detail("GotoKind", g.Kind.ToString()),
+                Detail("TargetName", g.Target.Name)
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildLabel(LabelExpression l, string path)
+    {
+        var children = new List<ExpressionNode>();
+        if (l.DefaultValue != null)
+        {
+            children.Add(BuildNode(l.DefaultValue, $"{path}/0"));
+        }
+
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Label",
+            NodeType = l.NodeType.ToString(),
+            Display = $"Label {l.Target.Name}",
+            TypeDisplay = SimplifyType(l.Type),
+            Details =
+            [
+                Detail("TargetName", l.Target.Name)
+            ],
+            Children = children
+        };
+    }
+
+    private static ExpressionNode BuildLoop(LoopExpression l, string path)
+    {
+        return new ExpressionNode
+        {
+            Path = path,
+            Kind = "Loop",
+            NodeType = l.NodeType.ToString(),
+            Display = "Loop",
+            TypeDisplay = SimplifyType(l.Type),
+            Details =
+            [
+                Detail("BreakLabel", l.BreakLabel?.Name ?? "-"),
+                Detail("ContinueLabel", l.ContinueLabel?.Name ?? "-")
+            ],
+            Children =
+            [
+                BuildNode(l.Body, $"{path}/0")
+            ]
         };
     }
 
--- ExpressionTreeExplorer.Tests/ExpressionNodeBuilderTests.cs
+++ ExpressionTreeExplorer.Tests/ExpressionNodeBuilderTests.cs
@@ -1,4 +1,5 @@
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 using ExpressionTreeExplorer.Core;
 
@@ -154,8 +155,8 @@ public class ExpressionNodeBuilderTests
     public void Build_UnknownNode_FallsBackGracefully()
     {
         var param = Expression.Parameter(typeof(User), "x");
-        var arrayExpr = Expression.NewArrayInit(typeof(int), Expression.Constant(1), Expression.Constant(2));
-        var lambda = Expression.Lambda<Func<User, int[]>>(arrayExpr, param);
+        var runtimeVars = Expression.RuntimeVariables(param);
+        var lambda = Expression.Lambda<Func<User, IRuntimeVariables>>(runtimeVars, param);
 
         var payload = ExpressionNodeBuilder.Build(lambda);
         var fallback = FindNodeByKind(payload.Roots[0], "Unsupported");
@@ -188,6 +189,215 @@ public class ExpressionNodeBuilderTests
         Assert.Equal("0/0", root.Children[0].Path);
         Assert.Equal("0/0/0", root.Children[0].Children[0].Path);
         Assert.Equal("0/0/1", root.Children[0].Children[1].Path);
+    }
+
+    [Fact]
+    public void Build_Conditional_HasThreeChildren()
+    {
+        var param = Expression.Parameter(typeof(User), "x");
+        var test = Expression.GreaterThan(Expression.Property(param, nameof(User.Age)), Expression.Constant(0));
+        var ifTrue = Expression.Property(param, nameof(User.Name));
+        var ifFalse = Expression.Constant("default");
+        var cond = Expression.Condition(test, ifTrue, ifFalse);
+        var lambda = Expression.Lambda<Func<User, string>>(cond, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Conditional");
+
+        Assert.NotNull(node);
+        Assert.Equal("Conditional", node.Kind);
+        Assert.Equal(3, node.Children.Count);
+    }
+
+    [Fact]
+    public void Build_New_SetsTypeName()
+    {
+        var param = Expression.Parameter(typeof(User), "x");
+        var newExpr = Expression.New(typeof(UserDto));
+        var lambda = Expression.Lambda<Func<User, UserDto>>(newExpr, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "New");
+
+        Assert.NotNull(node);
+        Assert.Equal("New", node.Kind);
+        Assert.Contains("UserDto", node.Display);
+    }
+
+    [Fact]
+    public void Build_TypeBinary_SetsTypeOperand()
+    {
+        var param = Expression.Parameter(typeof(object), "x");
+        var typeIs = Expression.TypeIs(param, typeof(User));
+        var lambda = Expression.Lambda<Func<object, bool>>(typeIs, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "TypeBinary");
+
+        Assert.NotNull(node);
+        Assert.Equal("TypeBinary", node.Kind);
+        Assert.Contains("User", node.Display);
+        Assert.Single(node.Children);
+    }
+
+    [Fact]
+    public void Build_NewArrayInit_HasElements()
+    {
+        var param = Expression.Parameter(typeof(User), "x");
+        var arrayExpr = Expression.NewArrayInit(typeof(int), Expression.Constant(1), Expression.Constant(2));
+        var lambda = Expression.Lambda<Func<User, int[]>>(arrayExpr, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "NewArray");
+
+        Assert.NotNull(node);
+        Assert.Equal("NewArray", node.Kind);
+        Assert.Equal(2, node.Children.Count);
+    }
+
+    [Fact]
+    public void Build_Default_SetsTypeName()
+    {
+        var param = Expression.Parameter(typeof(User), "x");
+        var defaultExpr = Expression.Default(typeof(int));
+        var lambda = Expression.Lambda<Func<User, int>>(defaultExpr, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Default");
+
+        Assert.NotNull(node);
+        Assert.Equal("Default", node.Kind);
+        Assert.Contains("int", node.Display);
+        Assert.Empty(node.Children);
+    }
+
+    [Fact]
+    public void Build_Block_HasExpressions()
+    {
+        var variable = Expression.Variable(typeof(int), "temp");
+        var assign = Expression.Assign(variable, Expression.Constant(42));
+        var block = Expression.Block(new[] { variable }, assign, variable);
+        var param = Expression.Parameter(typeof(User), "x");
+        var lambda = Expression.Lambda<Func<User, int>>(block, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Block");
+
+        Assert.NotNull(node);
+        Assert.Equal("Block", node.Kind);
+        Assert.Equal(3, node.Children.Count);
+    }
+
+    [Fact]
+    public void Build_TryCatch_HasHandlers()
+    {
+        var body = Expression.Constant(1);
+        var catchBody = Expression.Constant(0);
+        var handler = Expression.Catch(typeof(Exception), catchBody);
+        var tryExpr = Expression.TryCatch(body, handler);
+        var param = Expression.Parameter(typeof(User), "x");
+        var lambda = Expression.Lambda<Func<User, int>>(tryExpr, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Try");
+
+        Assert.NotNull(node);
+        Assert.Equal("Try", node.Kind);
+        Assert.Equal(2, node.Children.Count);
+        Assert.Equal("CatchBlock", node.Children[1].Kind);
+    }
+
+    [Fact]
+    public void Build_Switch_HasCases()
+    {
+        var param = Expression.Parameter(typeof(User), "x");
+        var switchValue = Expression.Property(param, nameof(User.Age));
+        var case1 = Expression.SwitchCase(Expression.Constant("young"), Expression.Constant(18));
+        var case2 = Expression.SwitchCase(Expression.Constant("old"), Expression.Constant(65));
+        var switchExpr = Expression.Switch(switchValue, Expression.Constant("other"), case1, case2);
+        var lambda = Expression.Lambda<Func<User, string>>(switchExpr, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Switch");
+
+        Assert.NotNull(node);
+        Assert.Equal("Switch", node.Kind);
+        Assert.Equal(4, node.Children.Count);
+        Assert.Equal("SwitchCase", node.Children[1].Kind);
+    }
+
+    [Fact]
+    public void Build_Goto_SetsTargetName()
+    {
+        var label = Expression.Label(typeof(int), "result");
+        var gotoExpr = Expression.Goto(label, Expression.Constant(42));
+        var labelExpr = Expression.Label(label, Expression.Constant(0));
+        var block = Expression.Block(gotoExpr, labelExpr);
+        var param = Expression.Parameter(typeof(User), "x");
+        var lambda = Expression.Lambda<Func<User, int>>(block, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Goto");
+
+        Assert.NotNull(node);
+        Assert.Equal("Goto", node.Kind);
+        Assert.Contains("result", node.Display);
+    }
+
+    [Fact]
+    public void Build_Label_SetsTargetName()
+    {
+        var label = Expression.Label(typeof(int), "result");
+        var labelExpr = Expression.Label(label, Expression.Constant(0));
+        var param = Expression.Parameter(typeof(User), "x");
+        var lambda = Expression.Lambda<Func<User, int>>(labelExpr, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Label");
+
+        Assert.NotNull(node);
+        Assert.Equal("Label", node.Kind);
+        Assert.Contains("result", node.Display);
+    }
+
+    [Fact]
+    public void Build_Loop_HasBody()
+    {
+        var breakLabel = Expression.Label("break");
+        var loop = Expression.Loop(Expression.Break(breakLabel), breakLabel);
+        var param = Expression.Parameter(typeof(User), "x");
+        var block = Expression.Block(loop, Expression.Default(typeof(int)));
+        var lambda = Expression.Lambda<Func<User, int>>(block, param);
+
+        var payload = ExpressionNodeBuilder.Build(lambda);
+        var node = FindNodeByKind(payload.Roots[0], "Loop");
+
+        Assert.NotNull(node);
+        Assert.Equal("Loop", node.Kind);
+        Assert.Single(node.Children);
+    }
+
+    [Fact]
+    public void Build_Lambda_HasReturnTypeDetail()
+    {
+        Expression<Func<User, bool>> expr = x => x.Age > 0;
+        var payload = ExpressionNodeBuilder.Build(expr);
+        var root = payload.Roots[0];
+
+        Assert.Contains(root.Details, d => d.Name == "ReturnType" && d.Value == "bool");
+        Assert.Contains(root.Details, d => d.Name == "ParameterCount" && d.Value == "1");
+    }
+
+    [Fact]
+    public void Build_Binary_HasIsLiftedDetail()
+    {
+        Expression<Func<User, bool>> expr = x => x.Age > 0;
+        var payload = ExpressionNodeBuilder.Build(expr);
+        var binary = FindNodeByKind(payload.Roots[0], "Binary");
+
+        Assert.NotNull(binary);
+        Assert.Contains(binary.Details, d => d.Name == "IsLifted");
+        Assert.Contains(binary.Details, d => d.Name == "Method");
     }
 
     #endregion
